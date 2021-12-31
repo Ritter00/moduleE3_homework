bd6d048f586ab665ebb8bbe1f40a0dbf8d039e97
Ritter00 <olzim@tut.by>
2021-12-31 11:34:56 +0300

    4

--- task_4_E4.js
+++ task_4_E4.js
@@ -1,3 +1,14 @@
 // Задание 4
+let x = +prompt('Введите начало отсчета');
+let y = +prompt('Введите конец отсчета');
+  
+let id = setInterval(printNumb, 1000);
 
-
+function printNumb (){
+  if (x <= y){
+    console.log(x);
+    x += 1;
+  } else {
+    clearInterval(id);
+  };
+};
